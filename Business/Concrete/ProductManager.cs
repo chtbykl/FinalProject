951c1bcef6afcd13fbad2ff954549b203239f3e2
Cihat Bıyıklı <chtbykl38@gmail.com>
2024-02-15 15:26:27 +0300

    15,02,2024 1526

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
+using ValidationException = FluentValidation.ValidationException;
 
 namespace Business.Concrete
 {
-    public class ProductManager:IProductService
+    public class ProductManager : IProductService
     {
         private IProductDal _productDal;
 
@@ -22,44 +27,51 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public IDataResult<Product> GetById(int id) 
+        public IDataResult<Product> GetById(int id)
         {
-            return new SuccessDataResult<Product>(_productDal.Get(p=> p.ProductId == id));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == id));
         }
+
+        [Validate]
         public IResult add(Product product)
         {
+            //validation
             //magic strings
-            if (product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.InvalidProductName);
-            }
+            // float validation
+
+            //cross cutting concerns: log, cache, transaction, authorization, validation, ...
+
+
+            ValidationTool.Validate(new ProductValidator(), product);
+
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetAll()
         {
+            //validation
             //iş kodları
-            if (DateTime.Now.Hour==20)
+            if (DateTime.Now.Hour == 20)
             {
                 return new ErrorDataResult<List<Product>>("bakım saati, lütfen daha sonra tekrar deneyiniz");
             }
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),"ürünler listelendi") ;
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), "ürünler listelendi");
         }
 
         public IDataResult<List<Product>> GetByCategoryId(int id)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=> p.CategoryId == id),"ürünler kategoriye göre listelendi");
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id), "ürünler kategoriye göre listelendi");
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal minPrice, decimal maxPrice)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= minPrice && p.UnitPrice <= maxPrice),"ürünler fiyatına göre listelendi");
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= minPrice && p.UnitPrice <= maxPrice), "ürünler fiyatına göre listelendi");
         }
 
         public IDataResult<List<ProductDetailDTO>> GetProductDetails()
         {
-            return new SuccessDataResult<List<ProductDetailDTO>>(_productDal.GetProductDetails(),"ürünler ayrıntılarıyla birlikte listelendi");
+            return new SuccessDataResult<List<ProductDetailDTO>>(_productDal.GetProductDetails(), "ürünler ayrıntılarıyla birlikte listelendi");
         }
     }
 }
